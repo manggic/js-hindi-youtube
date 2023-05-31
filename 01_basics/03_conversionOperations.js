a8de00d77bf8a87bb6cb022278df7607b6df4565
manish mahto <49410917+manggic@users.noreply.github.com>
2023-05-31 16:58:15 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,14 @@
+
+let score = "33abc"
+let scoreInNumber = Number(score)
+console.log(scoreInNumber); // NaN 
+
+// Number("33abc") => NaN
+// Number("33") => 33
+// Number(null) => 0
+// Number(undefined) => NaN
+// Number('') => 0
+
+
+
+
