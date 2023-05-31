655a95b193bbe2e71570f0169ffb4f32e97f42b7
manish mahto <49410917+manggic@users.noreply.github.com>
2023-05-31 13:58:22 +0000

    done with variable tutorial

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+
+
+const accountId = 222334
+let accountEmail = 'manish@m.com'
+var accountPass = '12345'
+accountCity = 'rajasthan'
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPass, accountCity ]);
+
+
+
+
